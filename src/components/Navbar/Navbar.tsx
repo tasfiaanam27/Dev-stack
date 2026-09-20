fe74dfb4c37f6951842066bb96b17e3b4fae8de9
Tasfia Anam <tasfia.anam2002@gmail.com>
2026-09-20 18:56:58 +0600

    Initialize Dev-stack project

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h2 className="text-3xl font-bold text-purple-600">
+        Dev Stack
+      </h2>
+    </nav>
+  );
+};
+
+export default Navbar;
